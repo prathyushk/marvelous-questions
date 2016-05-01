9de6c63116834f5027aefc7177e1a000aed60abc
Prathyush Katukojwala <prathyushk@ucla.edu>
2016-05-01 17:28:49 -0400

    Updated port

--- Server.js
+++ Server.js
@@ -181,8 +181,8 @@ app.use("*",function(req,res){
 	res.sendFile(path + "404.html");
     });
 
-app.listen(3000,function(){
-	console.log("Live at Port 3000");
+app.listen(process.env.PORT,function(){
+	console.log("Live!");
     });
 
 function getCreator(name,consideredChars,callback){
