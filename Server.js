040131421bba576d008ef9e5658862ddf1d46100
Prathyush Katukojwala <prathyushk@ucla.edu>
2016-05-01 07:49:00 -0400

    Polish and added more cross-linking

--- Server.js
+++ Server.js
@@ -2,27 +2,24 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var distFinder = require("./public/js/levenshtein");
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var privkey = require("./public/js/privatekey");
 var md5 = require('md5');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 var pubkey = "70a96fc75f1557c73820b896ec806902";
-var privkey = require("./public/js/privatekey");
-var help = "I have no idea what you're trying to ask me.<br>Try asking me things about Marvel events, comics, characters, or creators";
+var help = "Sorry, I couldn't understand your question.<br> Please try rephrasing or try one of these questions: <br>'Who is Iron Man?'<br>'What Comics was Hulk in?'<br>'Show me a picture of spider-man'<br>'Who was in the infinity wars?'<br>'Who wrote the unbeatable squirrel girl?'";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/static', express.static('public'));
 
-process.on('uncaughtException', function(ex) {
-	// do something with exception
-    });
+process.on('uncaughtException', function(ex) {console.log(ex);});
 
 app.post('/processq', function(req, res) {
 	var toParse = req.body.question;
 	var msftget = "https://api.projectoxford.ai/luis/v1/application?id=1207bd94-2b1e-49b6-a9f8-7bb18b367aa8&subscription-key=1aced98869eb4596948bc2d3daafff1e&q="+encodeURIComponent(toParse);
-	try{
 	makeXMLHttpRequest(msftget,function(){
 	    var resp = JSON.parse(this.responseText);
 	    console.log(resp);
@@ -31,14 +28,18 @@ app.post('/processq', function(req, res) {
 		    getCharacter(resp["entities"][0].entity,3,function(character){
 				if(character["name"] != ""){
 				    var comics = character["comics"]["items"];
+				    if(comics.length == 0){
+					res.send("Sorry, no comics were found to contain "+character["name"]);
+					return;
+				    }
 				    var str = "Some of the comics " + character["name"] + " was in: <br>";
 				    for(var c = 0,l=comics.length;c<l;c++){
-					str += comics[c]["name"]+"<br>";
+					str += "<a href='javascript:void(0);' onclick='showComic("+'"'+comics[c]["name"]+'"'+");'>"+comics[c]["name"]+"</a><br>";
 				    }
 				    res.send(str);
 				}
 				else
-				    res.send("I don't know which character you're talking about");
+				    res.send("Sorry, I couldn't find the character " + resp["entities"][0].entity);
 			    });
 		}
 		else
@@ -51,12 +52,12 @@ app.post('/processq', function(req, res) {
 				var characters = comic["characters"]["items"];
 				var str = "Some characters that were in " + comic["title"] + ": <br>";
 				for(var c = 0,l=characters.length;c<l;c++){
-				    str += characters[c]["name"] + '<br>';
+				    str += "<a href='javascript:void(0);' onclick='showCharacter("+'"'+characters[c]["name"]+'"'+");'>"+characters[c]["name"] + '</a><br>';
 				}
 				res.send(str);
 			    }
 			    else
-				res.send("I don't know which comic you're talking about");
+				res.send("Sorry, I couldn't find the comic " +resp["entities"][0].entity);
 			});
 		}	
 		else
@@ -67,14 +68,14 @@ app.post('/processq', function(req, res) {
 		    getEvent(resp["entities"][0].entity,function(event){
 			    if(event["title"] != ""){
 				var characters = event["characters"]["items"];
-				var str = "Some characters that were involved in " + event["title"] + ": <br>";
+				var str = event["description"]+"<br><br>Some characters that were involved in " + event["title"] + ": <br>";
 				for(var c = 0,l=characters.length;c<l;c++){
-                                    str += characters[c]["name"] + '<br>';
+                                    str += "<a href='javascript:void(0);' onclick='showCharacter("+'"'+characters[c]["name"]+'"'+");'>"+characters[c]["name"] + '</a><br>';
 				}
                                 res.send(str);
 			    }
 			    else
-				res.send("I don't know which event you're talking about");
+				res.send("Sorry, I couldn't find the event " + resp["entities"][0].entity);
 			});
 		}
 		else
@@ -87,12 +88,12 @@ app.post('/processq', function(req, res) {
 				var events = character["events"]["items"];
 				var str = "Some events that " + character["name"] + " was involved in: <br>";
 				for(var c=0,l=events.length;c<l;c++){
-				    str += events[c]["name"] + '<br>';
+				    str += "<a href='javascript:void(0);' onclick='showEvent("+'"'+events[c]["name"]+'"'+");'>"+events[c]["name"] + '</a><br>';
 				}
 				res.send(str);
 			    }
 			    else
-				res.send("I don't know which character you're talking about");
+				res.send("Sorry, I couldn't find the character " + resp["entities"][0].entity);
 			});
 		}
 		else
@@ -100,17 +101,17 @@ app.post('/processq', function(req, res) {
 	    }
 	    else if(resp["intents"][0]["intent"] == "CoBCr"){
 		if(resp["entities"].length > 0 && resp["entities"][0].type == "Creator"){
-		    getCreator(resp["entities"][0].entity,function(creator){
+		    getCreator(resp["entities"][0].entity,5,function(creator){
 			    if(creator["fullName"] != ""){
 				var comics = creator["comics"]["items"];
 				var str = "Some comics that " + creator["fullName"] + " wrote: <br>";
 				for(var c =0,l=comics.length;c<l;c++){
-				    str += comics[c]["name"] + '<br>';
+				    str += "<a href='javascript:void(0);' onclick='showComic("+'"'+comics[c]["name"]+'"'+");'>"+comics[c]["name"]+"</a><br>";
 				}
 				res.send(str);
 			    }
 			    else
-				res.send("I don't know which creator you are talking about");
+				res.send("Sorry, I couldn't find the creator " + resp["entities"][0].entity);
 			});
 		}
 		else
@@ -122,35 +123,39 @@ app.post('/processq', function(req, res) {
 			    if(character["name"] != ""){
 				if(character["description"] == ""){
 				    var comics = character["comics"]["items"];
-				    var str = "<div class='row'><div style='padding-right:0;' class='col-md-3'><img style='width:100%; ' src='"+character["thumbnail"]["path"]+"/portrait_medium."+character["thumbnail"]["extension"]+"'></div><div class='col-md-9'><h4>"+character["name"]+"</h4>"+ "Sorry, I don't have a description for " +character["name"] +"<br>But, here are some of the comics " + character["name"] + " was in: <br>";
+				    if(comics.length == 0){
+					res.send("Sorry, no comics were found to contain this character");
+					return;
+				    }
+				    var str = "<div class='row'><div style='padding-right:0;' class='col-md-3'><img style='width:100%; ' src='"+character["thumbnail"]["path"]+"/portrait_medium."+character["thumbnail"]["extension"]+"'></div><div class='col-md-9'><a href='"+character["urls"][0]["url"]+"' target='_blank'><h4>"+character["name"]+"</h4></a>"+ "Sorry, I don't have a description for " +character["name"] +"<br>But, here are some of the comics " + character["name"] + " was in: <br>";
 				    for(var c = 0,l=comics.length;c<l;c++){
-					str += comics[c]["name"] + '<br>';
+					str += "<a href='javascript:void(0);' onclick='showComic("+'"'+comics[c]["name"]+'"'+");'>"+comics[c]["name"]+"</a><br>";
 				    }
 				    res.send(str+"</div></div>");
 				}
 				else
-				    res.send("<div class='row'><div style='padding-right:0;' class='col-md-3'><img style='width:100%; ' src='"+character["thumbnail"]["path"]+"/portrait_medium."+character["thumbnail"]["extension"]+"'></div><div class='col-md-9'><h4>"+character["name"]+"</h4>"+character["description"]+"</div></div>");
+				    res.send("<div class='row'><div style='padding-right:0;' class='col-md-3'><img style='width:100%; ' src='"+character["thumbnail"]["path"]+"/portrait_medium."+character["thumbnail"]["extension"]+"'></div><div class='col-md-9'><a href='"+character["urls"][0]["url"]+"' target='_blank'><h4>"+character["name"]+"</h4></a>"+character["description"]+"</div></div>");
 			    }
 			    else
-				res.send("I don't know which character you're talking about");
+				res.send("Sorry, I couldn't find the character " + resp["entities"][0].entity);
 			});
 		}
 		else
 		    res.send(help);
 	    }
 	    else if(resp["intents"][0]["intent"] == "CrBCo"){
-		if(resp["entities"].length > 0 && resp["entities"][0].type == "Comic"){
+		if(resp["entities"].length > 0 && (resp["entities"][0].type == "Comic" || resp["entities"][0].type == "CharacterName")){
 		    getComic(resp["entities"][0].entity,5,function(comic){
 			    if(comic["title"] != ""){
 				var creators = comic["creators"]["items"];
-				var str = "The creator(s) of " + comic["title"] + ": <br>";
+				var str = "<div class='row'><div style='padding-right:0;' class='col-md-5'><img style='width:100%;' src='"+comic["thumbnail"]["path"]+"/portrait_medium."+comic["thumbnail"]["extension"]+"'></div><div class='col-md-7'><a href='"+comic["urls"][0]["url"]+"' target='_blank'><h4>"+comic["title"]+"</h4></a>The creator(s): <br>";
 				for(var c=0,l=creators.length;c<l;c++){
-				    str += creators[c]["name"] + '<br>';
+				    str += "<a href='javascript:void(0);' onclick='showCreator("+'"'+creators[c]["name"]+'"'+");'>"+creators[c]["name"] + '</a><br>';
 				}
-				res.send(str);
+				res.send(str+"</div></div>");
 			    }
 			    else
-				res.send("I don't know which comic you're talking about");
+				res.send("Sorry, I couldn't find the comic " +resp["entities"][0].entity);
 			});
 		}
 		else
@@ -159,8 +164,6 @@ app.post('/processq', function(req, res) {
 	    else
 		res.send(help);
 	    });
-	}
-	catch(err){}
     });
 
 router.use(function (req,res,next) {
@@ -182,17 +185,23 @@ app.listen(3000,function(){
 	console.log("Live at Port 3000");
     });
 
-function getCreator(name,callback){
-    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/creators?nameStartsWith="+name.charAt(0)+"&limit=100",function(){
+function getCreator(name,consideredChars,callback){
+    if(name.length < consideredChars)
+	consideredChars = name.length;
+    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/creators?orderBy=firstName&nameStartsWith="+name.substr(0,consideredChars)+"&limit=100",function(){
 	    var creators = JSON.parse(this.responseText)["data"]["results"];
-	    callback(getClosestCreator(name,creators));
+	    var closest = getClosestCreator(name,creators);
+	    if(closest["fullName"] == "" && consideredChars > 1)
+		getCreator(name,consideredChars-1,callback);
+	    else
+		callback(closest);
 	});
 }
 
 function getCharacter(name,consideredChars,callback){
     if(name.length < consideredChars)
 	consideredChars = name.length;
-    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/characters?nameStartsWith="+name.substr(0,consideredChars)+"&limit=100",function(){
+    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/characters?orderBy=name&nameStartsWith="+name.substr(0,consideredChars)+"&limit=100",function(){
 	    var chars = JSON.parse(this.responseText)["data"]["results"];
 	    var closestChar = getClosestCharacter(name,chars);
 	    if(closestChar["name"] == "" && consideredChars > 1)
@@ -205,7 +214,7 @@ function getCharacter(name,consideredChars,callback){
 function getComic(name,consideredChars,callback){
     if(name.length < consideredChars)
         consideredChars = name.length;
-    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/comics?titleStartsWith="+name.substr(0,consideredChars)+"&limit=100",function(){
+    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/comics?orderBy=title&titleStartsWith="+name.substr(0,consideredChars)+"&limit=100",function(){
             var comics = JSON.parse(this.responseText)["data"]["results"];
 	    var closest = getClosestComic(name,comics);
 	    if(closest["title"] == "" && consideredChars > 1)
@@ -216,7 +225,7 @@ function getComic(name,consideredChars,callback){
 }
 
 function getEvent(name,callback){
-    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/events?nameStartsWith="+name.charAt(0)+"&limit=100",function(){
+    makeMarvelRequest("http://gateway.marvel.com:80/v1/public/events?orderBy=name&nameStartsWith="+name.charAt(0)+"&limit=100",function(){
 	    var events = JSON.parse(this.responseText)["data"]["results"];
 	    callback(getClosestEvent(name,events));
 	});
